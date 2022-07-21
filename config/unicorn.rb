ffe27bfced55c00ac15e82a917c3e9102fc6a9ae
yuki saito <ykst0622@gmail.com>
2022-07-21 15:34:16 +0900

    Update unicorn config to listen on NGINX socket.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,15 +1,15 @@
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
-timeout 15
-preload_app true
-worker_processes 4
-listen 'unix:///tmp/nginx.socket', backlog: 1024
+# worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+# timeout 15
+# preload_app true
+# worker_processes 4
+# listen 'unix:///tmp/nginx.socket', backlog: 1024
 
-before_fork do |server,worker|
-    FileUtils.touch('/tmp/app-initialized')
-end
-
-# require 'fileutils'
-# listen '/tmp/nginx.socket'
 # before_fork do |server,worker|
-# FileUtils.touch('/tmp/app-initialized')
+#     FileUtils.touch('/tmp/app-initialized')
 # end
+
+require 'fileutils'
+listen '/tmp/nginx.socket'
+before_fork do |server,worker|
+FileUtils.touch('/tmp/app-initialized')
+end
